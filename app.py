520085205e9edd4b949f4299616474a2b60d6e06
Nickolay <kolaz92@gmail.com>
2024-07-04 21:22:04 +0300

    req file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pickle as pkl
 import streamlit as st
-import joblib
-import sklearn
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.probability import FreqDist
+# import joblib
+# import sklearn
+# import nltk
+# from nltk.tokenize import word_tokenize
+# from nltk.probability import FreqDist
 import pandas as pd
 
 st.set_page_config(
